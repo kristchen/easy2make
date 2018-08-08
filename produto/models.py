2117056d3cfb3ae8a71783a89f4465e3b7bf1bdc
kristchen <kristchen@yahoo.com.br>
2018-08-07 21:24:32 -0300

    adicionar menu e arquivos estaticos

--- produto/models.py
+++ produto/models.py
@@ -1,10 +1,13 @@
 from django.db import models
-
+from django.urls import reverse
 
 class Produto(models.Model):
     descricao = models.CharField(max_length=255, null=False)
     preco = models.DecimalField(max_digits=10, decimal_places=2, null=False)
-    marca = models.CharField(max_length=255, null=True)
-    modelo = models.CharField(max_length=255, null=True)
-    imagem = models.ImageField(null=True)
+    marca = models.CharField(max_length=255, blank=True, default='')
+    modelo = models.CharField(max_length=255, blank=True, default='')
+    imagem = models.ImageField(blank=True)
+
+    def get_absolute_url(self):
+        return reverse('produto:adicionar')
 
--- produto/views.py
+++ produto/views.py
@@ -5,4 +5,8 @@ from .models import Produto
 class ProdutoCreate(CreateView):
     model = Produto
     fields = ['descricao','marca','modelo','imagem','preco']
-    template_name = 'adicionar_produto_form.html'
+    template_name = 'adicionar_produto_form.html'       
+
+    def form_invalid(self, form):
+        form.save(commit=True)
+        return super(ProdutoCreate, self).form_valid(form)
