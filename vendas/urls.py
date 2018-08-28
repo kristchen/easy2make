40c13f65e7816e6892e192b1919439ff33aaf240
kristchen <kristchen@yahoo.com.br>
2018-08-27 21:30:25 -0300

    Alteracao da tela de vendas

--- vendas/urls.py
+++ vendas/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-from .views import VendasCreateForm, VendasCreateAPI, ItemVendaCreateAPI
+from .views import VendasCreateForm, ItemVendaDeleteAPI, VendasCreateAPI, ItemVendaCreateAPI, VendasDeleteAPI
 
 urlpatterns = [
     path('', VendasCreateForm.as_view(), name='vendas-create-form'),
     path('adicionar/', VendasCreateAPI.as_view()),
-    path('itens/adicionar/', ItemVendaCreateAPI.as_view())
+    path('remover/<int:pk>/', VendasDeleteAPI.as_view()),
+    path('itens/adicionar/', ItemVendaCreateAPI.as_view()),
+    path('itens/remover/<int:pk>/', ItemVendaDeleteAPI.as_view())
 ]
 
--- vendas/views.py
+++ vendas/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
 from json_views.views import JSONFormView
 from .forms import VendasForm, ItemVendaForm
-from .models import Venda
+from .models import Venda, ItemVenda
+from django.views.generic.detail import SingleObjectMixin
 
 class VendasCreateForm(TemplateView):
     template_name = 'vendas_form.html'
@@ -13,20 +14,33 @@ class VendasCreateAPI(JSONFormView):
     def post(self, request, *args, **kwargs):
         return __post__(self, VendasCreateAPI, request, args, kwargs)
 
+class VendasDeleteAPI(JSONFormView):
+
+    form_class = VendasForm    
+    def post(self, request, *args, **kwargs):
+        venda = Venda.objects.get(pk=kwargs['pk'])
+        venda.delete()
+        return self.render_to_response(self.get_context_data(sucess=True))
+
 class ItemVendaCreateAPI(JSONFormView):
    
     form_class = ItemVendaForm
     def post(self, request, *args, **kwargs):
         return __post__(self, ItemVendaCreateAPI, request, args, kwargs)
 
+class ItemVendaDeleteAPI(JSONFormView):
+
+    form_class = ItemVendaForm
+    def post(self, request, *args, **kwargs):
+        item = ItemVenda.objects.get(pk=kwargs['pk'])
+        item.delete()
+        return self.render_to_response(self.get_context_data(sucess=True))
 
 def __post__(self, classView, request, args, kwargs):
     
     form = self.form_class(request.POST)
-    print(request.POST)
     if form.is_valid():
         return self.render_to_response(self.get_context_data(id=form.save().id, sucess=True))
     return super(classView, self).post(self, request, args, kwargs)
-
 
  
